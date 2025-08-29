e43899a332772502c07cb893521a1cccc1c994f9
Valeria <valeriapaz010203@outlook.com>
2025-08-28 21:12:15 -0500

    now yes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
     // El estado loginData se utiliza para almacenar los datos del formulario (email, password).
